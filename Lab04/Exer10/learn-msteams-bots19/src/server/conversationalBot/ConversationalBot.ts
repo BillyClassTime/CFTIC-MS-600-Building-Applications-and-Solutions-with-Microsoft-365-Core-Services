0e9b8d8df2757a8a668a163f93adbc9dd1f4ef34
Billy Vanegas <billyvb2004@gmail.com>
2021-09-24 12:29:58 +0200

    Al final del ejercicio 11

--- Lab04/Exer10/learn-msteams-bots19/src/server/conversationalBot/ConversationalBot.ts
+++ Lab04/Exer10/learn-msteams-bots19/src/server/conversationalBot/ConversationalBot.ts
@@ -138,11 +138,11 @@ export class ConversationalBot extends TeamsActivityHandler {
         });
 
         this.onMessageReaction(async (context: TurnContext): Promise<void> => {
-            const added = context.activity.reactionsAdded;
-            if (added && added[0]) {
-                await context.sendActivity({
-                    textFormat: "xml",
-                    text: `That was an interesting reaction (<b>${added[0].type}</b>)`
+           if (context.activity.reactionsAdded) {
+                context.activity.reactionsAdded.forEach(async (reaction) => {
+                    if (reaction.type === "like") {
+                        await context.sendActivity("Thank you!");
+                    }
                 });
             }
         });
